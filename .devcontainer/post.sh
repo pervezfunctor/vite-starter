951ee103be906c27e0ac051bbabee23cae0e487a
Pervez Iqbal <pervezfunctor@gmail.com>
2022-08-03 11:09:33 +0000

    use spaceship theme for zsh

--- .devcontainer/post.sh
+++ .devcontainer/post.sh
@@ -1,9 +1,7 @@
-#! /usr/bin/env bash
-
 sclone() {
     local dest=${*: -1}
     [ -d "$dest" ] && return
-    git clone "$@"
+    git clone --depth=1 "$@"
 }
 
 [ -d $HOME/.oh-my-zsh ] || sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -11,9 +9,12 @@ sclone() {
 sclone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sclone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+sclone https://github.com/spaceship-prompt/spaceship-prompt.git "$HOME/.spaceship-prompt"
+ln -s "$HOME/.spaceship-prompt/spaceship.zsh-theme" ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship.zsh-theme
+
 git config pull.rebase true
 git config --global core.autocrlf true
 
-cp .devcontainer/.zshrc $HOME"
+cp .devcontainer/.zshrc $HOME
 
 pnpm install
